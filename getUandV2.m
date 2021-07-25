f2a632554bade080e208ef3fd9aa877d14c29afe
Christian Engel <32718302+chrsengel@users.noreply.github.com>
2021-07-25 14:59:59 +0200

    Delete old file

--- getUandV2.m
+++
@@ -1,8 +0,0 @@
-function [u,v] = getUandV2(K1_inv, K2_inv, T, x1)
-    % in case two inverse Ks, a point x1
-    % and the translation is given
-    
-    x = (inv(K2_inv) * (K1_inv * x1 * T)) / 4;
-    u = x(1);
-    v = x(2);
-end
