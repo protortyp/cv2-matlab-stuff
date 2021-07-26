c9f45d6fa7aa7bbbf3cf275afdef6c5d60672a19
Christian Engel <engel.christian1994@gmail.com>
2021-07-26 08:02:51 +0200

    Modify getEpipolarLineEquation to adjust for uncalibrated case

--- app_calc_ep_eq.m
+++ app_calc_ep_eq.m
@@ -1,11 +1,23 @@
 clear;
 
 % Exam P9 c)
-% for a calibrated case where K1=K2=I
-T=[-1;3;3];
-x1=[2;2;1];
+T = [-1; 3; 3];
+x1 = [2; 2; 1];
 R = eye(3);
 
-[m,b] = getEpipolarLineEquation(T, R, x1);
+% for uncalibrated case:
+K1 = [5 0 0;
+    0 10 0;
+    0 0 1];
+K2 = [2 0 0
+    0 5 0
+    0 0 1];
+
+% for calibrated case:
+%K1=eye(3);
+%K2=eye(3);
+
+[m, b] = getEpipolarLineEquation(T, R, x1, K1, K2);
+
 fprintf('m = %s\n', rats(m));
 fprintf('b = %s\n', rats(b));
